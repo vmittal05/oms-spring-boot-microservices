8583a9f19443b8ef84b4ecb81ac18d20249fcc3f
Vaibhav Mittal <vaibhavmittal0506@gmail.com>
2024-09-18 00:11:59 +0530

    api gateway added

---
+++ api-gateway/src/test/java/com/oms/gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oms.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- order-service/src/test/java/com/microservices/order/OrderServiceApplicationTests.java
+++ order-service/src/test/java/com/microservices/order/OrderServiceApplicationTests.java
@@ -5,15 +5,20 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.cloud.contract.wiremock.AutoConfigureWireMock;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MySQLContainer;
+
+import com.microservices.order.stubs.InventoryClientStubs;
+
 import org.hamcrest.Matchers;
 import static org.hamcrest.MatcherAssert.*;
 
 import io.restassured.RestAssured;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
+// @Import(TestcontainersConfiguration.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWireMock(port = 0)
 class OrderServiceApplicationTests {
 
 	@ServiceConnection
@@ -37,11 +42,13 @@ class OrderServiceApplicationTests {
 	void shouldSubmitOrder(){
 		String submitOrderJson = """
 				{
-				"skuCode": "Iphone 16",
+				"skuCode": "Iphone 14",
 				"price": 999,
 				"quantity": 1
 				}
 				""";
+
+		InventoryClientStubs.stubInventoryCall("IPhone 14", 10);
 		var responseBodyString = RestAssured.given().contentType("application/json")
 								.body(submitOrderJson)
 								.when().post("/api/order").then()
