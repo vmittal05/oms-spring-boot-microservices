9edd407f5dea14dfa5ca53979991d5b17258120a
Vaibhav Mittal <vaibhavmittal0506@gmail.com>
2024-09-28 20:43:17 +0530

    circuit breaker added in api-gateway

--- api-gateway/src/main/java/com/oms/gateway/routes/Routes.java
+++ api-gateway/src/main/java/com/oms/gateway/routes/Routes.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctio
 import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.function.HandlerFunction;
 import org.springframework.web.servlet.function.RequestPredicate;
 import org.springframework.web.servlet.function.RequestPredicates;
@@ -13,50 +14,77 @@ import org.springframework.web.servlet.function.ServerResponse;
 import static org.springframework.cloud.gateway.server.mvc.filter.FilterFunctions.setPath;
 import static org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions.route;
 
+import java.net.URI;
+
+import org.springframework.cloud.gateway.server.mvc.filter.CircuitBreakerFilterFunctions;
+
 @Configuration
 public class Routes {
-    @Bean  
-    public RouterFunction<ServerResponse> productServiceRoute(){
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute() {
         return GatewayRouterFunctions.route("product_service")
-            .route(RequestPredicates.path("/api/products"), HandlerFunctions.http("http://localhost:8080"))
-            .build();
+                .route(RequestPredicates.path("/api/products"), HandlerFunctions.http("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> productServiceSwaggerRoute(){
+    public RouterFunction<ServerResponse> productServiceSwaggerRoute() {
         return GatewayRouterFunctions.route("product_service_swagger")
-            .route(RequestPredicates.path("/aggeregate/product-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8080"))
-            .filter(setPath("/api-docs"))
-            .build();
+                .route(RequestPredicates.path("/aggeregate/product-service/v3/api-docs"),
+                        HandlerFunctions.http("http://localhost:8080"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("productServiceSwaggerCircuitBreaker",
+                URI.create("forward:/fallbackRoute")))
+                .filter(setPath("/api-docs"))
+                .build();
     }
 
-    @Bean  
-    public RouterFunction<ServerResponse> inventoryServiceRoute(){
+    @Bean
+    public RouterFunction<ServerResponse> inventoryServiceRoute() {
         return GatewayRouterFunctions.route("inventory_service")
-            .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
-            .build();
+                .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute(){
+    public RouterFunction<ServerResponse> inventoryServiceSwaggerRoute() {
         return GatewayRouterFunctions.route("inventory_service_swagger")
-            .route(RequestPredicates.path("/aggeregate/inventory-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8082"))
-            .filter(setPath("/api-docs"))
-            .build();
-    }
-
-    @Bean  
-    public RouterFunction<ServerResponse> orderServiceRoute(){
-        return GatewayRouterFunctions.route("order_service")
-            .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
-            .build();
+                .route(RequestPredicates.path("/aggeregate/inventory-service/v3/api-docs"),
+                        HandlerFunctions.http("http://localhost:8082"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("inventoryServiceSwaggerCircuitBreaker",
+                URI.create("forward:/fallbackRoute")))
+                .filter(setPath("/api-docs"))
+                .build();
     }
 
     @Bean
-    public RouterFunction<ServerResponse> orderServiceSwaggerRoute(){
+    public RouterFunction<ServerResponse> orderServiceRoute() {
+        return GatewayRouterFunctions.route("order_service")
+                .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceCircuitBreaker",
+                        URI.create("forward:/fallbackRoute")))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderServiceSwaggerRoute() {
         return GatewayRouterFunctions.route("order_service_swagger")
-            .route(RequestPredicates.path("/aggeregate/order-service/v3/api-docs"), HandlerFunctions.http("http://localhost:8081"))
-            .filter(setPath("/api-docs"))
-            .build();
+                .route(RequestPredicates.path("/aggeregate/order-service/v3/api-docs"),
+                        HandlerFunctions.http("http://localhost:8081"))
+                .filter(CircuitBreakerFilterFunctions.circuitBreaker("orderServiceSwaggerCircuitBreaker",
+                URI.create("forward:/fallbackRoute")))
+                .filter(setPath("/api-docs"))
+                .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> fallBackRoute() {
+        return route("fallbackRoute")
+                .GET("/fallbackRoute",
+                        request -> ServerResponse.status(HttpStatus.SERVICE_UNAVAILABLE).body("Service Unavailable"))
+                .build();
     }
 }
