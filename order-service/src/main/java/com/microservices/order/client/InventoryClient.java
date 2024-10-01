69c0113a7d3ef73afdf1aca70287ccc4fa64a346
vmittal05 <vaibhavmittal0506@gmail.com>
2024-10-01 23:55:29 +0530

    added circuit-breaker to order-service

--- order-service/src/main/java/com/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/microservices/order/client/InventoryClient.java
@@ -1,13 +1,26 @@
 package com.microservices.order.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
-// @FeignClient(value = "inventory", url = "${inventory.url}")
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
+// @FeignClient(value = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
+
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
 
     // @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skucode {}, failure reason: {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
